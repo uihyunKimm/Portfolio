44756d532202195d808df870507c1be5a7cdda64
uihyunkimm <dmlxld@gmail.com>
2024-03-28 22:43:03 +0900

    App.js setting complete loading check

--- src/App.js
+++ src/App.js
@@ -1,84 +1,17 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./components/Root";
-import Home from "./components/Home";
-import NotFound from "./components/NotFound";
-import Product from "./components/Product";
-import ProductInfo from "./components/ProductInfo";
+import Layout from "./components/Layout";
+import HomeContainer from "./components/HomeContainer";
 
-/*
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Home</div>,
-    errorElement: <div>Page Not Found</div>,
-  },
-  {
-    path: "/product",
-    element: <div>Product</div>,
-  },
-]);
-*/
-
-/*
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/product",
-    element: <Product />,
-    errorElement: <NotFound />,
-  },
-]);
-*/
-
-/*
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/product",
-    element: <Product />,
-    errorElement: <NotFound />,
-  },
-]);
-*/
-
-/*
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "/product", element: <Product /> },
-    ],
-  },
-]);
-*/
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <NotFound />,
-    children:[
-      { index: true, element: <Home /> },//index true는 기본값 설정과
-      { path: "/product", element: <Product /> },
-      { path: "/product/:productId", element: <ProductInfo /> },
-    ]
-  }
-]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+        <Layout>
+          <HomeContainer/>
+        </Layout>
+    </>
+  );
 };
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,20 @@ import React from "react";
 const Header = () => {
     return (
         <header className="Header-module">
-            홈
+            <div>
+                <div>
+                    로고
+                </div>
+                <div>
+                    정보
+                </div>
+                <nav>
+                    <ul>
+                        <li>메뉴</li>
+                        <li>메뉴</li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 };
