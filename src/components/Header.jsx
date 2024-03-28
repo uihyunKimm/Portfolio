0d21894506965f83d69aecfd3d3d514bd41484a6
uihyunkimm <dmlxld@gmail.com>
2024-03-28 22:03:49 +0900

    Portfolio first

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="Header-module">
+            홈
+        </header>
+    );
+};
+
+export default Header;
