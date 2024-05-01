a995ed58c2d8a3c4d2a47a8cf50397f8706478df
uihyunkimm <dmlxld@gmail.com>
2024-05-01 23:48:10 +0900

    feat: footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,9 +13,57 @@ const Footer = () => {
                     <h3 className={styles.title}>푸터임</h3>
                     <h3 className={styles.title}>bbq 따라서 만들거임</h3>
                 </div>
+                <ul className='footer-right'>
+                  <li>
+                    <img src={TelIcon} alt="tel" />
+                    TEL : +82 ) 010-XXXX-XXXX
+                  </li>
+                  <li>
+                    <img src={MailIcon} alt="mail" />
+                    E-Mail : hotbody@gmail.com
+                  </li>
+                </ul>
             </div> 
         </footer>
     );
 };
-
+/* 
 export default Footer;
+
+const Footer = () => {
+    return (
+      <div className="footer-wrap">
+        <div className="footer-container">
+          <div className='footer-left'>
+            <h1>HOT BODY</h1>
+  
+            <div className="footer-mid">
+              <ul className="footer-menu-lists">
+                <li>About</li>
+                <li>Community</li>
+                <li>PT</li>
+                <li>Group</li>
+                <li>Subscribe</li>
+                <li>Profile</li>
+              </ul>
+              <p>&copy; 2024 All right reserved by hot body</p>
+            </div>
+          </div>
+  
+          <ul className='footer-right'>
+            <li>
+              <img src={TelIcon} alt="tel" />
+              TEL : +82 ) 010-XXXX-XXXX
+            </li>
+            <li>
+              <img src={MailIcon} alt="mail" />
+              E-Mail : hotbody@gmail.com
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Footer;
+   */
