ae87e21a92ccbf3d87282b598ef56a3dc75c4021
uihyunkimm <dmlxld@gmail.com>
2024-05-19 15:21:36 +0900

    feat: Add card lotate logic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,10 +11,10 @@ const Card = () => {
         setX(e.clientX);
         setY(e.clientY);
         console.log(x,y)
-        /* let rotateY = 1/5 * x + 20;
+        let rotateY = 1/5 * x + 20;
         let rotateX = 4/30 * y + 20;
         e.target.style = `transform : rotateY(${rotateY}deg)`
-        e.target.style = `transform : rotateX(${rotateX}deg)` */
+        e.target.style = `transform : rotateX(${rotateX}deg)`
 }
     return (
         <div className={styles.main2 }>
