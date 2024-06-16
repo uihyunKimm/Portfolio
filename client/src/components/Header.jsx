df28d2f37d0d857390eca43b19cc4fcf4879f5a7
uihyunkimm <dmlxld@gmail.com>
2024-06-16 20:37:22 +0900

    feat: Add useRef effect

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -40,49 +40,42 @@ if (headerToggle) {
 const Header = () => {
   const [show, setShow] = useState(false);
 
-    const toggleMenu = () => {
-        setShow((prevShow) => !prevShow);
-    };
+  const toggleMenu = () => {
+    setShow((prevShow) => !prevShow);
+  };
     return (
       <header id={styles.header} role="banner">
-          <div className={styles.container}>
-            <div className={styles.contents}>
-                <Link to="/">로고</Link>
-                <a href="mailto:dmlxld@gmail.com" target="_blank">dmlxld@gmail.com</a>
-                <p><span className={styles.navigation}>2024 <strong>Portfolio</strong></span></p>
-            </div>
-            <div className={styles.contents_low}>
-              <span className={styles.contents_low_low}>화면기획/ 웹디자인 출신 개발자</span>
-              <nav className={styles.header__nav} role="navigation" aria-label="메인 메뉴">
-                    <ul>
-                        {headerNav.map((nav, key) => (
-                            <li key={key}>
-                                <a href={nav.url}>{nav.title}</a>
-                            </li>
-                        ))}
-                    </ul>
-              {/*   <ul>
-                  <li><Link to="#intro">intro</Link></li>
-                  <li><Link to="#skill">skill</Link></li>
-                  <li><Link to="#site">site</Link></li>
-                  <li><Link to="#port">portfolio</Link></li>
-                  <li><Link to="#contact">contact</Link></li>
-                </ul> */}
-              </nav>
-              <div 
-                className={styles.header__nav__mobile}
-                id="headerToggle" 
-                aria-controls="primary-menu" 
-                aria-expanded={show ? "true" : "false"}
-                role="button" 
-                tabindex="0"
-                onClick={toggleMenu}
-                >
-                <span></span>
-              </div>
+        <div className={styles.container}>
+          <div className={styles.contents}>
+            <a href="/">로고</a>
+            <a href="mailto:dmlxld@gmail.com" target="_blank">dmlxld@gmail.com</a>
+            <p><span className={styles.navigation}>2024 <strong>Portfolio</strong></span></p>
+          </div>
+          <div className={styles.contents_low}>
+            <span className={styles.contents_low_low}>화면기획/ 웹디자인 출신 개발자</span>
+            <nav className={`${styles.header__nav} ${show ? "show" : ""}`} role="navigation" aria-label="메인 메뉴">
+              <ul>
+                {headerNav.map((nav, key) => (
+                  <li key={key}>
+                      <a href={nav.url}>{nav.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            <div 
+              className={styles.header__nav__mobile}
+              id="headerToggle" 
+              aria-controls="primary-menu" 
+              aria-expanded={show ? "true" : "false"}
+              role="button" 
+              tabindex="0"
+              onClick={toggleMenu}
+              >
+              <span></span>
             </div>
           </div>
-        </header>
+        </div>
+      </header>
     );
 };
 
