ac190dba07007b4d7b229fba2487191943d5cbfe
uihyunkimm <dmlxld@gmail.com>
2024-06-16 19:19:56 +0900

    feat: Fix hover action

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,6 +2,33 @@ import React from "react";
 import styles from "./../style/Footer.module.scss"
 
 const Footer = () => {
+  const footerText = [
+      {
+          title: "youtube",
+          desc: "유튜브에 오시면 더 많은 강의를 볼 수 있습니다.",
+          link: "https://www.youtube.com/@Webstoryboy",
+      },
+      {
+          title: "github",
+          desc: "깃헙에 오시면 더 많은 소스를 볼 수 있습니다.",
+          link: "https://github.com/webstoryboy",
+      },
+      {
+          title: "blog",
+          desc: "블러그에 오시면 더 많은 정보를 볼 수 있습니다.",
+          link: "https://webstoryboy.co.kr",
+      },
+      {
+          title: "gsap",
+          desc: "GSAP에 오시면 더 많은 강의를 볼 수 있습니다.",
+          link: "https://www.youtube.com/playlist?list=PL4UVBBIc6giL8-6jvrClimg0cFL-Muqiq",
+      },
+      {
+          title: "react",
+          desc: "리액트로 만든 사이트를 같이 만들어 봅니다.",
+          link: "https://github.com/webstoryboy/port2023-react",
+      }
+  ];
     return (
         <footer className={styles.footer__body} >
           <div className={styles.container}>
@@ -20,83 +47,24 @@ const Footer = () => {
                 <div className={styles.right}>
                   <h3>social</h3>
                   <ul className={styles.footer_right}>
-                    <li>
-                      <span>github</span>
-                      <em>깃헙에 들어오시면 모든 소스를 볼 수 있습니다.</em>
-                    </li>
-                    <li>
-                      <span>tistory</span>
-                      <em>티스토리에 들어오시면 좋은 정보 볼 수 있습니다.</em>
-                    </li>
-                    <li>
-                      <span>gsap</span>
-                      <em>GSAP를 공부하시고 오면 도움이 됩니다.</em>
-                    </li>
-                    <li>
-                      <span>vite</span>
-                      <em>비트 강의도 곧 오픈 예정입니다.</em>
-                    </li>
-                    <li>
-                      <span>react</span>
-                      <em>리액트 강의도 곧 오픈 예정입니다.</em>
-                    </li>
-                    <li>
-                      <span>vue</span>
-                      <em>뷰 강의도 곧 오픈 예정입니다.</em>
-                    </li>
-                    <li>
-                      <span>next</span>
-                      <em>넥스트 강의도 곧 오픈 예정입니다.</em>
-                    </li>
+                    {footerText.map((footer, key) => (
+                      <li key={key}>
+                          <a href={footer.link}>{footer.title}</a>
+                          <em>{footer.desc}</em>
+                      </li>
+                    ))}
                   </ul>
                 </div>
               </div>
               <div className={styles.footer__right}>
-                  © 2024 kimuihyun<br />
-                  {/* 이 사이트는 비트를 이용하여 제작하였습니다. */}
+                © 2024 kimuihyun<br />
+                이 사이트는 리액트를 이용하여 제작하였습니다.
               </div>
             </div>
           </div>
         </footer>
     );
 };
-/* 
-export default Footer;
 
-const Footer = () => {
-    return (
-      <div className="footer-wrap">
-        <div className="footer-container">
-          <div className='footer-left'>
-            <h1>HOT BODY</h1>
-  
-            <div className="footer-mid">
-              <ul className="footer-menu-lists">
-                <li>About</li>
-                <li>Community</li>
-                <li>PT</li>
-                <li>Group</li>
-                <li>Subscribe</li>
-                <li>Profile</li>
-              </ul>
-              <p>&copy; 2024 All right reserved by hot body</p>
-            </div>
-          </div>
-  
-          <ul className='footer-right'>
-            <li>
-              <img src={TelIcon} alt="tel" />
-              TEL : +82 ) 010-XXXX-XXXX
-            </li>
-            <li>
-              <img src={MailIcon} alt="mail" />
-              E-Mail : hotbody@gmail.com
-            </li>
-          </ul>
-        </div>
-      </div>
-    );
-  };
-   */
   export default Footer;
   
